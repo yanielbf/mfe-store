e0dbcb0e86c64214b58356912fa151a522023e9c
Yaniel Blanco Fernandez <c.yblanco@shipbob.com>
2023-09-04 13:57:03 -0300

    Remove share

--- host-app/vite.config.js
+++ host-app/vite.config.js
@@ -4,6 +4,9 @@ import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  },
   plugins: [
     vue(),
     federation({
